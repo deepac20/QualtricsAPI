c03937ae4044761de0a41a176e57bf6adcfe55e7
Deepa <dxc496@case.edu>
2019-08-09 20:56:51 -0400

    added function

--- cleanData.py
+++ cleanData.py
@@ -3,24 +3,18 @@ import glob
 import pandas as pd
 
 dir = os.getcwd()
-os.chdir(dir + "/MyQualtricsDownload/TechSupport Intervention")
+os.chdir(dir + "/MyQualtricsDownload/TechSupport/Intervention")
 extension = "csv"
 
 all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
-# combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames ])
-# export to csv
-# combined_csv.to_csv( "combined_csv.csv", index=False, encoding='utf-8-sig')
 
-
-#print all_filenames
 for i,file in enumerate(all_filenames):
- #   print file
     module = "HTN" in file and "HTN" or "PPT"
-    fname = file.strip("TechSuPPort_-HTN_-_")
+    fname = file.replace("TechSuPPort_-HTN_-_",'')
     fname = fname.replace(' + INTRO','')
     os.rename(file,fname)
     all_filenames[i] = fname
-    week = fname[6:7]
+    week = fname[5:6]
     df = pd.read_csv(fname)
     df['Module'] = module
     df['Week'] = week
--- main.py
+++ main.py
@@ -9,6 +9,57 @@ import sys
 # Setting user Parameters
 
 
+def filedownload(survey, study):
+    DESTINATION_PATH = "MyQualtricsDownload/" + study
+
+    for surveyId, group in survey.items():
+        # Setting static parameters
+        requestCheckProgress = 0.0
+        progressStatus = "inProgress"
+        baseUrl = "https://{0}.qualtrics.com/API/v3/surveys/{1}/export-responses/".format(dataCenter, surveyId)
+        headers = {
+            "content-type": "application/json",
+            "x-api-token": apiToken,
+        }
+
+        # Step 1: Creating Data Export
+        downloadRequestUrl = baseUrl
+        downloadRequestPayload = '{"format":"' + fileFormat + '"}'
+        downloadRequestResponse = requests.request("POST", downloadRequestUrl, data=downloadRequestPayload,
+                                                   headers=headers)
+        progressId = downloadRequestResponse.json()["result"]["progressId"]
+        print(downloadRequestResponse.text)
+
+        # Step 2: Checking on Data Export Progress and waiting until export is ready
+        while progressStatus != "complete" and progressStatus != "failed":
+            print ("progressStatus=", progressStatus)
+            requestCheckUrl = baseUrl + progressId
+            requestCheckResponse = requests.request("GET", requestCheckUrl, headers=headers)
+            requestCheckProgress = requestCheckResponse.json()["result"]["percentComplete"]
+            print("Download is " + str(requestCheckProgress) + " complete")
+            progressStatus = requestCheckResponse.json()["result"]["status"]
+
+        # step 2.1: Check for error
+        if progressStatus is "failed":
+            raise Exception("export failed")
+
+        fileId = requestCheckResponse.json()["result"]["fileId"]
+
+        # Step 3: Downloading file
+        requestDownloadUrl = baseUrl + fileId + '/file'
+        requestDownload = requests.request("GET", requestDownloadUrl, headers=headers, stream=True)
+
+        # Step 4: Unzipping the file
+        if group == "Intervention":
+            zipfile.ZipFile(io.BytesIO(requestDownload.content)).extractall(
+                DESTINATION_PATH+"/Intervention")
+            print('Complete')
+        else:
+            zipfile.ZipFile(io.BytesIO(requestDownload.content)).extractall(
+                DESTINATION_PATH+"/Controlled")
+            print('Complete')
+    return None
+
 apiToken = raw_input('Enter token: ')
 
 
@@ -27,90 +78,5 @@ techsupport_surveyId = {"SV_bQNtPmy7DLiJMR7":"Intervention","SV_d7pEOjmSwoaOyl7"
 fileFormat = "csv"
 dataCenter = 'cwru.az1'
 
-for surveyId, group in techsupport_surveyId.items():
-    # Setting static parameters
-    requestCheckProgress = 0.0
-    progressStatus = "inProgress"
-    baseUrl = "https://{0}.qualtrics.com/API/v3/surveys/{1}/export-responses/".format(dataCenter, surveyId)
-    headers = {
-        "content-type": "application/json",
-        "x-api-token": apiToken,
-    }
-
-    # Step 1: Creating Data Export
-    downloadRequestUrl = baseUrl
-    downloadRequestPayload = '{"format":"' + fileFormat + '"}'
-    downloadRequestResponse = requests.request("POST", downloadRequestUrl, data=downloadRequestPayload, headers=headers)
-    progressId = downloadRequestResponse.json()["result"]["progressId"]
-    print(downloadRequestResponse.text)
-
-    # Step 2: Checking on Data Export Progress and waiting until export is ready
-    while progressStatus != "complete" and progressStatus != "failed":
-        print ("progressStatus=", progressStatus)
-        requestCheckUrl = baseUrl + progressId
-        requestCheckResponse = requests.request("GET", requestCheckUrl, headers=headers)
-        requestCheckProgress = requestCheckResponse.json()["result"]["percentComplete"]
-        print("Download is " + str(requestCheckProgress) + " complete")
-        progressStatus = requestCheckResponse.json()["result"]["status"]
-
-    # step 2.1: Check for error
-    if progressStatus is "failed":
-        raise Exception("export failed")
-
-    fileId = requestCheckResponse.json()["result"]["fileId"]
-
-    # Step 3: Downloading file
-    requestDownloadUrl = baseUrl + fileId + '/file'
-    requestDownload = requests.request("GET", requestDownloadUrl, headers=headers, stream=True)
-
-    # Step 4: Unzipping the file
-    if group == "Intervention":
-        zipfile.ZipFile(io.BytesIO(requestDownload.content)).extractall("MyQualtricsDownload/TechSupport/Intervention")
-        print('Complete')
-    else:
-        zipfile.ZipFile(io.BytesIO(requestDownload.content)).extractall("MyQualtricsDownload/TechSupport/Controlled")
-        print('Complete')
-
-for surveyId, group in coachman_surveyId.items():
-    # Setting static parameters
-    requestCheckProgress = 0.0
-    progressStatus = "inProgress"
-    baseUrl = "https://{0}.qualtrics.com/API/v3/surveys/{1}/export-responses/".format(dataCenter, surveyId)
-    headers = {
-        "content-type": "application/json",
-        "x-api-token": apiToken,
-    }
-
-    # Step 1: Creating Data Export
-    downloadRequestUrl = baseUrl
-    downloadRequestPayload = '{"format":"' + fileFormat + '"}'
-    downloadRequestResponse = requests.request("POST", downloadRequestUrl, data=downloadRequestPayload, headers=headers)
-    progressId = downloadRequestResponse.json()["result"]["progressId"]
-    print(downloadRequestResponse.text)
-
-    # Step 2: Checking on Data Export Progress and waiting until export is ready
-    while progressStatus != "complete" and progressStatus != "failed":
-        print ("progressStatus=", progressStatus)
-        requestCheckUrl = baseUrl + progressId
-        requestCheckResponse = requests.request("GET", requestCheckUrl, headers=headers)
-        requestCheckProgress = requestCheckResponse.json()["result"]["percentComplete"]
-        print("Download is " + str(requestCheckProgress) + " complete")
-        progressStatus = requestCheckResponse.json()["result"]["status"]
-
-    # step 2.1: Check for error
-    if progressStatus is "failed":
-        raise Exception("export failed")
-
-    fileId = requestCheckResponse.json()["result"]["fileId"]
-
-    # Step 3: Downloading file
-    requestDownloadUrl = baseUrl + fileId + '/file'
-    requestDownload = requests.request("GET", requestDownloadUrl, headers=headers, stream=True)
-
-    # Step 4: Unzipping the file
-    if group == "Intervention":
-        zipfile.ZipFile(io.BytesIO(requestDownload.content)).extractall("MyQualtricsDownload/Coachman/Intervention")
-        print('Complete')
-    else:
-        zipfile.ZipFile(io.BytesIO(requestDownload.content)).extractall("MyQualtricsDownload/Coachman/Controlled")
-        print('Complete')
+filedownload(techsupport_surveyId,"TechSupport")
+filedownload(coachman_surveyId, "Coachman")
